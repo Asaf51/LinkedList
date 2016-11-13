4f0d9157825cdd4be709dbd9ac5e55cfb54fcfe0
Asaf Zoler <aszoler@gmail.com>
2016-11-13 17:03:46 +0200

    add_end() method added

--- List.c
+++ List.c
@@ -19,6 +19,7 @@ void error(char *message);
 List *init();
 void print_list(List *list);
 void add_beginning(List *list, int val);
+void add_end(List *list, int val);
 
 void main(void)
 {
@@ -26,7 +27,24 @@ void main(void)
     add_beginning(list, 5);
     add_beginning(list, 7);
     add_beginning(list, 8);
+    add_end(list, 10);
     print_list(list);
+}
+
+void add_end(List *list, int val)
+{
+    // If it's the first node
+    if (list->first == NULL)
+        add_beginning(list, val);
+    else
+    {
+        Node *new_node = (Node *)ec_malloc(sizeof(List));
+        new_node->value = val;
+        new_node->previous = list->last;
+        new_node->next = NULL;
+        list->last->next = new_node;
+        list->last = new_node;
+    }
 }
 
 void add_beginning(List *list, int val)
@@ -46,6 +64,7 @@ void add_beginning(List *list, int val)
     {
         new_node->next = NULL;
         list->first = new_node;
+        list->last = new_node;
     }
 }
 
@@ -54,7 +73,7 @@ void print_list(List *list)
     Node *current = list->first;
     while (current != NULL)
     {
-        printf("<- %d -> ", current->value);
+        printf("%d -> ", current->value);
         current = (Node *)current->next;
     }
 }
