782c759c260755dc605cfb461f4c8355dac6204b
Asaf Zoler <aszoler@gmail.com>
2016-11-16 22:03:30 +0200

    get_sum() added and refactoring

--- List.c
+++ List.c
@@ -3,76 +3,81 @@
 void main(void)
 {
     // Initiate the list 
-    List *list = init();
+    list_t *list = init();
 
     int i;
 
     for (i = 1; i <= 10; ++i)
     {
-        add_end(list, i);
+        add_node(list, i, get_last(list));
     }
 
-    Node *curr = get_first(list);
+    node_t *curr = get_first(list);
 
     for (i = 1; i < 4; ++i)
     {
-        curr = next(curr);
+        curr = get_next(curr);
     }
 
     remove_node(list, curr);
 
+    //destroy(list);
+
     // And print it
     print_list(list);
 
-    destroy(list);
-
-    printf("%d\n", get_length(list));
+    printf("%d\n", get_sum(list));
 }
 
-void destroy(List *list)
+int32_t get_sum(list_t *list)
 {
-    Node *curr_node = get_first(list);
-    while(next(curr_node) != NULL)
+    return list->sum;
+}
+
+void destroy(list_t *list)
+{
+    node_t *curr_node = get_first(list);
+    while(get_next(curr_node) != NULL)
     {
-        curr_node = next(curr_node);
-        free(previous(curr_node));
+        curr_node = get_next(curr_node);
+        free(get_previous(curr_node));
     }
 
     free(list);
 }
 
-Node *next(Node *node)
+node_t *get_next(node_t *node)
 {
     return node->next;
 }
 
-Node *previous(Node *node)
+node_t *get_previous(node_t *node)
 {
     return node->previous;
 }
 
-int get_length(List *list)
+uint32_t get_length(list_t *list)
 {
     return list->length;
 }
 
-Node *get_first(List *list)
+node_t *get_first(list_t *list)
 {
     return list->first;
 }
 
-Node *get_last(List *list)
+node_t *get_last(list_t *list)
 {
     return list->last;
 }
 
-int remove_node(List *list, Node *node)
+int32_t remove_node(list_t *list, node_t *node)
 {
-    int value = node->value;
+    int32_t value = node->value;
 
     if(list->first == node)
     {
-        Node *next_node = node->next;
+        node_t *next_node = node->next;
         free(node);
 
         list->first = next_node;
@@ -81,15 +86,15 @@ int remove_node(List *list, Node *node)
     }
     else if(list->last == node)
     {
-        Node *previous_node = node->previous;
+        node_t *previous_node = node->previous;
 
         previous_node->next = NULL;
         list->last = previous_node;
     } 
     else
     {
-        Node *previous_node = node->previous;
-        Node *next_node = node->next;
+        node_t *previous_node = node->previous;
+        node_t *next_node = node->next;
         free(node);
 
         next_node->previous = previous_node;
@@ -97,9 +102,10 @@ int remove_node(List *list, Node *node)
     }
 
     list->length--;
+    list->sum -= value;
 }
 
-void add_node(List *list, int val, Node *node)
+void add_node(list_t *list, int32_t val, node_t *node)
 {
     // If the given node is the last node
     if (list->last == node) 
@@ -112,16 +118,17 @@ void add_node(List *list, int val, Node *node)
     }
     else // Add the node between two existing nodes
     {
-        Node *new_node = (Node *)ec_malloc(sizeof(Node));
+        node_t *new_node = (node_t *)ec_malloc(sizeof(node_t));
         new_node->value = val;
         new_node->previous = node;
         new_node->next = node->next;
         node->next = new_node;
         list->length++;
+        list->sum += val;
     }
 }
 
-void add_end(List *list, int val)
+void add_end(list_t *list, int32_t val)
 {
     // If it's the first node
     if (list->first == NULL)
@@ -130,20 +137,21 @@ void add_end(List *list, int val)
     }
     else
     {
-        Node *new_node = (Node *)ec_malloc(sizeof(Node));
+        node_t *new_node = (node_t *)ec_malloc(sizeof(node_t));
         new_node->value = val;
         new_node->previous = list->last;
         new_node->next = NULL;
         list->last->next = new_node;
         list->last = new_node;
         list->length++;
+        list->sum += val;
     }
 }
 
-void add_beginning(List *list, int val)
+void add_beginning(list_t *list, int32_t val)
 {
-    Node *first_node = list->first;
-    Node *new_node = (Node *)ec_malloc(sizeof(Node));
+    node_t *first_node = list->first;
+    node_t *new_node = (node_t *)ec_malloc(sizeof(node_t));
     new_node->value = val;
     new_node->previous = NULL;
 
@@ -154,6 +162,7 @@ void add_beginning(List *list, int val)
         new_node->next = first_node;
         first_node->previous = new_node;
         list->length++;
+        list->sum += val;
     } 
     else
     {
@@ -161,31 +170,34 @@ void add_beginning(List *list, int val)
         list->first = new_node;
         list->last = new_node;
         list->length++;
+        list->sum += val;
     }
 }
 
-void print_list(List *list)
+void print_list(list_t *list)
 {
-    Node *current = get_first(list);
+    node_t *current = get_first(list);
     while (current != NULL)
     {
-        printf("%d -> ", value(current));
-        current = next(current);
+        printf("%d -> ", get_value(current));
+        current = get_next(current);
     }
     printf("\n");
 }
 
-int value(Node *node)
+int32_t get_value(node_t *node)
 {
     return node->value;
 }
 
 // Allocationg and initiate a new list and returns a list pointer.
-List *init()
+list_t *init()
 {
-    List *list = (List *)ec_malloc(sizeof(List));
+    list_t *list = (list_t *)ec_malloc(sizeof(list_t));
     list->first = NULL;
     list->last = NULL;
+    list->length = 0;
+    list->sum = 0;
 }
 
 // Error Check malloc, gets size and return the pointer.
--- List.h
+++ List.h
@@ -3,34 +3,36 @@
 #include <stdint.h>
 #include <string.h>
 
-typedef struct Node{
-    struct Node *previous;
-    int value;
-    struct Node *next;
-} Node;
+typedef struct node_s{
+    struct node_s *previous;
+    int32_t value;
+    struct node_s *next;
+} node_t;
 
-typedef struct {
-    Node *first;
-    Node *last;
-    int length;
-} List;
+typedef struct list_s {
+    node_t *first;
+    node_t *last;
+    uint32_t length;
+    int32_t sum;
+} list_t;
 
 void *ec_malloc(uint16_t size);
 void error(char *message);
 
-List *init();
+list_t *init();
 
-void print_list(List *list);
+void print_list(list_t *list);
 
-void add_beginning(List *list, int val);
-void add_end(List *list, int val);
-void add_node(List *list, int val, Node *node);
-int remove_node(List *list, Node *node);
-void destroy(List *list);
+void add_beginning(list_t *list, int32_t val);
+void add_end(list_t *list, int32_t val);
+void add_node(list_t *list, int32_t val, node_t *node);
+int32_t remove_node(list_t *list, node_t *node);
+void destroy(list_t *list);
 
-Node *get_first(List *list);
-Node *get_last(List *list);
-int get_length(List *list);
-Node *previous(Node *node);
-Node *next(Node *node);
-int value(Node *node);
+node_t *get_first(list_t *list);
+node_t *get_last(list_t *list);
+uint32_t get_length(list_t *list);
+node_t *get_previous(node_t *node);
+node_t *get_next(node_t *node);
+int32_t get_value(node_t *node);
+int32_t get_sum(list_t *list);
